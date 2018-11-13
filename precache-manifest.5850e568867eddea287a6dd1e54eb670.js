6d786f25132599fbed60626ab8f042e96a45a46e
Kehinde Egunjobi <kehindeegunjobi93@yahoo.com>
2018-11-13 09:32:19 +0100

    Updates

---
+++ precache-manifest.5850e568867eddea287a6dd1e54eb670.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5bee33ad84f16ca2cf0f",
+    "url": "/react-todo/static/js/runtime~main.5bee33ad.js"
+  },
+  {
+    "revision": "0f2a269cd792d707482a",
+    "url": "/react-todo/static/js/main.0f2a269c.chunk.js"
+  },
+  {
+    "revision": "872633813eb3847f201b",
+    "url": "/react-todo/static/js/1.87263381.chunk.js"
+  },
+  {
+    "revision": "0f2a269cd792d707482a",
+    "url": "/react-todo/static/css/main.46946d97.chunk.css"
+  },
+  {
+    "revision": "0a4aea41d24a14e216ab4d0c85284c32",
+    "url": "/react-todo/index.html"
+  }
+];
